ebac86e14384c764e0fd477afff0c291d3df299f
ludoo0d0a <ludoo0d0a@users.noreply.github.com>
2025-12-18 18:08:13 +0000

    deploy: 8a005bbe540afd50e7083916fcd44e67bf518c8b

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/telma/sw.js', { scope: '/telma/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
