c1e2bb3fb902d246761fa7fe560f31411bc7eaa0
LudoO <ludovic.valente@gmail.com>
2025-12-18 19:06:13 +0100

    refactor(vite.config): update base URL configuration for deployment
    - Changed the base URL in Vite configuration to use the root path instead of a specific subdirectory, facilitating easier deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
       },
     }),
   ],
-  base: process.env.PUBLIC_URL || '/telma/',
+  base: process.env.PUBLIC_URL || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
